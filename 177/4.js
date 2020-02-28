e2b624527a65d15affe77e382da33b5582487cfb
ljb <ljb_wh@hotmail.com>
2020-02-29 10:55:49 +1300

    all done for 177th week contest

--- 177/4.js
+++ 177/4.js
@@ -3,50 +3,51 @@
  * @return {string}
  */
 var largestMultipleOfThree = function (digits) {
-    const cnt = digits.reduce((t, i) => {
-        t[i] = t[i] + 1;
-        return t
-    }, new Array(10).fill(0));
-    console.log(`${JSON.stringify(digits)} count ${JSON.stringify(cnt)}`);
+  const cnt = digits.reduce((t, i) => {
+    t[i] = t[i] + 1
+    return t
+  }, new Array(10).fill(0))
+  console.log(`${JSON.stringify(digits)} count ${JSON.stringify(cnt)}`)
 
-    let remain1Cnt = cnt[1] + cnt[4] + cnt[7]; // Number of elements with remainder = 1
-    let remain2Cnt = cnt[2] + cnt[5] + cnt[8]; // Number of elements with remainder = 2
-    const remainSum = (remain1Cnt + 2 * remain2Cnt) % 3;
-    if (remainSum == 1) { // Delete 1 smallest digit with remainder = 1 or Delete 2 smallest digits with remainder = 2
-        if (remain1Cnt >= 1) remain1Cnt -= 1;
-        else remain2Cnt -= 2;
-    } else if (remainSum == 2) { // Delete 1 smallest digit with remainder = 2 or Delete 2 smallest digits with remainder = 1
-        if (remain2Cnt >= 1) remain2Cnt -= 1;
-        else remain1Cnt -= 2;
+  let remain1Cnt = cnt[1] + cnt[4] + cnt[7] // Number of elements with remainder = 1
+  let remain2Cnt = cnt[2] + cnt[5] + cnt[8] // Number of elements with remainder = 2
+  const remainSum = (remain1Cnt + 2 * remain2Cnt) % 3
+  if (remainSum == 1) {
+    // Delete 1 smallest digit with remainder = 1 or Delete 2 smallest digits with remainder = 2
+    if (remain1Cnt >= 1) remain1Cnt -= 1
+    else remain2Cnt -= 2
+  } else if (remainSum == 2) {
+    // Delete 1 smallest digit with remainder = 2 or Delete 2 smallest digits with remainder = 1
+    if (remain2Cnt >= 1) remain2Cnt -= 1
+    else remain1Cnt -= 2
+  }
+
+  const sb = new Array()
+  for (let d = 9; d >= 0; d--) {
+    if (d % 3 == 0) {
+      while (cnt[d]-- > 0) {
+        sb.push(d)
+      }
+    } else if (d % 3 == 1) {
+      while (cnt[d]-- > 0 && remain1Cnt-- > 0) {
+        sb.push(d)
+      }
+    } else {
+      while (cnt[d]-- > 0 && remain2Cnt-- > 0) {
+        sb.push(d)
+      }
     }
+  }
+  let result = sb.join('')
+  if (sb.length > 0 && sb[0] == '0') {
+    result = '0'
+  } // Remove leading 0 case [0,...,0]
+  console.log(`${JSON.stringify(digits)} return ${result}`)
 
-    const sb = new Array();
-    for (let d = 9; d >= 0; d--) {
-        if (d % 3 == 0) {
-            while (cnt[d]-- > 0) {
-                sb.push(d);
-            }
-        } else if (d % 3 == 1) {
-            while (cnt[d]-- > 0 && remain1Cnt-- > 0) {
-                sb.push(d);
-            }
-        } else {
-            while (cnt[d]-- > 0 && remain2Cnt-- > 0) {
-                sb.push(d);
-            }
-        }
-    }
-    let result = sb.join('');
-    if (sb.length > 0 && sb[0] == '0') {
-        result = "0"
-    }; // Remove leading 0 case [0,...,0]
-    console.log(`${JSON.stringify(digits)} return ${result}`);
+  return result
+}
 
-    return result;
-};
-
-
-largestMultipleOfThree([8, 1, 9]);
-largestMultipleOfThree([8, 6, 7, 1, 0]);
-largestMultipleOfThree([1]);
-largestMultipleOfThree([0, 0, 0, 0, 0, 0]);
+largestMultipleOfThree([8, 1, 9])
+largestMultipleOfThree([8, 6, 7, 1, 0])
+largestMultipleOfThree([1])
+largestMultipleOfThree([0, 0, 0, 0, 0, 0])
--- README.md
+++ README.md
@@ -10,6 +10,6 @@ A record and review for my journey of leetcode week contest
 Week 177th
 
 - [x] [1360. number of days between two dates](./177/1.md)
-- [ ] [1361. validate binary tree nodes](./177/2.md)
-- [ ] [1362. closest divisors](./177/3.md)
-- [ ] [1363. largest multiple of three](./177/4.md)
+- [x] [1361. validate binary tree nodes](./177/2.md)
+- [x] [1362. closest divisors](./177/3.md)
+- [x] [1363. largest multiple of three](./177/4.md)
